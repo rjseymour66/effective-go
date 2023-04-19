490a425c4b9330b06a22a89a6e097f8a4edb4a03
rseymour <rjs.github@gmail.com>
2023-04-18 23:41:51 -0400

    implemented logger

--- shortner/cmd/shortd/shortd.go
+++ shortner/cmd/shortd/shortd.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"effective-go/shortner/internal/httpio"
 	"effective-go/shortner/short"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -16,6 +18,9 @@ func main() {
 		timeout = 10 * time.Second
 	)
 
+	logger := log.New(os.Stderr, "shortner: ", log.LstdFlags|log.Lmsgprefix)
+	logger.Println("starting the server on", addr)
+
 	fmt.Fprintln(os.Stderr, "starting the server on", addr)
 
 	shortener := short.NewServer()
@@ -24,6 +29,11 @@ func main() {
 		Addr:        addr,
 		Handler:     http.TimeoutHandler(shortener, timeout, "timeout"),
 		ReadTimeout: timeout,
+	}
+
+	if os.Getenv("LINKIT_DEBUG") == "1" {
+		server.ErrorLog = logger
+		server.Handler = httpio.LoggingMiddleware(server.Handler)
 	}
 
 	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
--- shortner/internal/httpio/httpio.go
+++ shortner/internal/httpio/httpio.go
@@ -1,9 +1,11 @@
 package httpio
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"net/http"
+	"time"
 )
 
 // Decode reads from a reader into any value
@@ -18,4 +20,23 @@ func Encode(w http.ResponseWriter, code int, v any) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	return json.NewEncoder(w).Encode(v)
+}
+
+// LoggingMiddleware logs each handler response time.
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		end := time.Since(start)
+		Log(r.Context(), "%s %s %s %v", r.Method, r.URL.Path, r.RemoteAddr, end)
+	})
+}
+
+// Log is the custom logger implementation.
+func Log(ctx context.Context, format string, args ...any) {
+	s, _ := ctx.Value(http.ServerContextKey).(*http.Server)
+	if s == nil || s.ErrorLog == nil {
+		return
+	}
+	s.ErrorLog.Printf(format, args...)
 }
